9caf6c3c2760deb2c8c6de6ee3f12b7120ec6e3a
Avneet Singh <avneetsingh843@gmail.com>
2024-01-20 23:55:05 +0530

    legal aid structure

---
+++ src/app/Components/legalaid/LegalAidBody/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>
+      2
+    </div>
+  )
+}
+
+export default index
+2
---
+++ src/app/legalAid/page.jsx
@@ -0,0 +1,13 @@
+import  Header  from "../Components/legalaid/Header"
+import LegalAidBody from "../Components/legalaid/LegalAidBody"
+
+const page = () => {
+    return (
+        <div>
+            <Header/>
+            <LegalAidBody/>
+        </div>
+    )
+}
+
+export default page
