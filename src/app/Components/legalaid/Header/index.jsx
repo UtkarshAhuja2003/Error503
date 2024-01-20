e6096780802d328f5292c3177a14b9930d9ba231
Avneet Singh <avneetsingh843@gmail.com>
2024-01-21 01:44:22 +0530

    legal Aid

--- src/app/Components/legalaid/Header/index.jsx
+++ src/app/Components/legalaid/Header/index.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+"use client";
+import Image from 'next/image'
+import Link from 'next/link';
 
-const index = () => {
+function index(){
+
   return (
     <div>
-      1
+        <div className='md:flex flex-row-reverse w-[90%] justify-between mx-auto mt-12 md:mt-24'>
+            <div className='w-[100%] mx-auto md:w-[55%]'><Image className="w-[80%] mx-auto" src="/images/aid.jpg" alt="JoinUs Hero" width={1000} height={1000}></Image></div>
+            <div className='w-[90%] mx-auto md:w-[40%]'>
+                <div className='text-3xl md:text-5xl font-bold mt-6 md:mt-24 text-center md:text-left'>Join Our Team</div>
+                <div className='text-3xl md:text-5xl font-bold text-[#ffb500] text-center md:text-left mt-[20px]'>EL-FI Homes</div>
+                <div className=' text-[18px] mt-[20px] text-center md:text-left'>Work At The Most Dynamic And Successful Company</div>
+                <div className='md:w-[100%] mx-auto text-center md:text-left'>
+                    <Link href="/legalAid#legalAidBody"><button className='bg-[#ffb500] hover:bg-white hover:text-[#ffb500] border border-1 border-[#ffb500] ease-in-out duration-300 text-white rounded-md w-[144px] h-[40px] mt-6 mx-auto'>Get Aid</button></Link>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
+
+
+
+
 
 export default index
--- src/app/Components/legalaid/LegalAidBody/index.jsx
+++ src/app/Components/legalaid/LegalAidBody/index.jsx
@@ -1,12 +1,85 @@
+import Link from 'next/link'
 import React from 'react'
+import { MdFamilyRestroom } from "react-icons/md";
+import { PiStudentBold } from "react-icons/pi";
+import { IoMdHome } from "react-icons/io";
+import { FaRegMoneyBillAlt } from "react-icons/fa";
+import { MdHealthAndSafety } from "react-icons/md";
+
 
 const index = () => {
+  const cards = [
+    {
+      heading: "Employment",
+      text: "Employment Discrimination, Termination, Wage Claims, Criminal Records, More Topics",
+      link:"",
+      icon: MdFamilyRestroom,
+    },
+    {
+      heading: "Renters & Homeowners",
+      text: "Your rights as a renter, homeowner, or someone looking for a home",
+      link:"",
+      icon: PiStudentBold,
+    },
+    {
+      heading: "Your rights as a renter",
+      text: "Your rights as a student, employee, ex-employee, or person looking for a job in Georgia",
+      link:"",
+      icon: IoMdHome,
+    },
+    {
+      heading: "Money & Debt",
+      text: "Your rights when you have a legal issue involving money or debt",
+      link:"",
+      icon: FaRegMoneyBillAlt,
+    },
+    {
+      heading: "Health",
+      text: "Your right to apply for public benefits and appeal benefit decisions",
+      link:"",
+      icon: MdHealthAndSafety,
+    },
+  ]
   return (
-    <div>
-      2
+    <div className='flex text-[#04434E] mt-12'>
+      <div>
+      <div className='bg-[#A8C7E7] w-[30vw] m-[1rem] p-[1rem] rounded-lg mb-[0.7rem]'>
+        <div className='text-[#04434E] font-[800] text-xl'>Most Popular Topics</div>
+        <Link href="#">1. How do I evict a guest, roommate or family member?</Link>
+        <br/>
+        <Link href="#">2. What is a Power of Attorney?</Link>
+        <br/>
+        <Link href="#">3. FAQ: Guardianship and Conservatorship</Link>
+        <br/>
+
+        <div className='text-[#04434E] font-[800] text-xl mb-[0.7rem] mt-[0.7rem]'>Quick Links</div>
+        <Link href="#">1. FAQ: What if I'm Picked up by Immigration and Customs Enforcement (ICE) in DC?</Link>
+        <br/>
+        <Link href="#">2. FAQ: Late Fee Fairness Amendment Act Of 2016</Link>
+        <br/>
+      </div>
+      </div>
+      <div className=' w-[70vw] mx-[1rem] px-[1rem] py-[0.6rem] rounded-lg mb-[0.7rem]'>
+
+        <div className='flex flex-wrap justify-between'>
+          {
+            cards.map((card,i) => {
+              return <div key={i} className='w-[49%] rounded-lg px-4 py-4 my-2 bg-[#A8C7E7] text-[#04434E] hover:bg-[#04434E] duration-300 ease-in-out hover:bg-[#A8C7E7] hover:text-white'>
+                      <div className='flex'>
+                      {card.icon && React.createElement(card.icon, { className: 'text-3xl mt-1' })}
+                      <div className=' font-[800] text-xl px-2 text-2xl py-2'>{card.heading}</div>
+                       </div>
+                      <Link href={card.link}><p className='ml-2'>{card.text}</p></Link>
+                    </div>
+            })
+          }
+
+        </div>
+
+
+      </div>
     </div>
   )
 }
 
 export default index
-2
